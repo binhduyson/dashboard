cf03c9183e5edbba7424063080d34e4d733b0ffe
binhduyson <63969863+binhduyson@users.noreply.github.com>
2021-11-30 07:29:23 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,91 +1,127 @@
-document.querySelectorAll('.sidebar-submenu').forEach(e => {
-    e.querySelector('.sidebar-menu-dropdown').onclick = (event) => {
-        event.preventDefault()
-        e.querySelector('.sidebar-menu-dropdown .dropdown-icon').classList.toggle('active')
+document.querySelectorAll(".sidebar-submenu").forEach((e) => {
+  e.querySelector(".sidebar-menu-dropdown").onclick = (event) => {
+    event.preventDefault();
+    e.querySelector(".sidebar-menu-dropdown .dropdown-icon").classList.toggle(
+      "active"
+    );
 
-        let dropdown_content = e.querySelector('.sidebar-menu-dropdown-content')
-        let dropdown_content_lis = dropdown_content.querySelectorAll('li')
+    let dropdown_content = e.querySelector(".sidebar-menu-dropdown-content");
+    let dropdown_content_lis = dropdown_content.querySelectorAll("li");
 
-        let active_height = dropdown_content_lis[0].clientHeight * dropdown_content_lis.length
+    let active_height =
+      dropdown_content_lis[0].clientHeight * dropdown_content_lis.length;
 
-        dropdown_content.classList.toggle('active')
+    dropdown_content.classList.toggle("active");
 
-        dropdown_content.style.height = dropdown_content.classList.contains('active') ? active_height + 'px' : '0'
-    }
-})
+    dropdown_content.style.height = dropdown_content.classList.contains(
+      "active"
+    )
+      ? active_height + "px"
+      : "0";
+  };
+});
 
 let category_options = {
-    series: [44, 55, 41, 17],
-    labels: ['Cloths', 'Devices', 'Bags', 'Watches'],
-    chart: {
-        type: 'donut',
-    },
-    colors: ['#6ab04c', '#2980b9', '#f39c12', '#d35400']
-}
+  series: [44, 55, 41, 17],
+  labels: ["Cloths", "Devices", "Bags", "Watches"],
+  chart: {
+    type: "donut",
+  },
+  colors: ["#6ab04c", "#2980b9", "#f39c12", "#d35400"],
+};
 
-let category_chart = new ApexCharts(document.querySelector("#category-chart"), category_options)
-category_chart.render()
+let category_chart = new ApexCharts(
+  document.querySelector("#category-chart"),
+  category_options
+);
+category_chart.render();
 
 let customer_options = {
-    series: [{
-        name: "Store Customers",
-        data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
-    },{
-        name: "Online Customers",
-        data: [20, 30, 10, 20, 16, 40, 20, 51, 10]
-    }],
-    colors: ['#6ab04c', '#2980b9'],
-    chart: {
-        height: 350,
-        type: 'line',
+  series: [
+    {
+      name: "Store Customers",
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
     },
-    dataLabels: {
-        enabled: false
+    {
+      name: "Online Customers",
+      data: [20, 30, 10, 20, 16, 40, 20, 51, 10],
     },
-    stroke: {
-        curve: 'smooth'
-    },
-    xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-    },
-    legend: {
-        position: 'top'
-    }
-}
+  ],
+  colors: ["#6ab04c", "#2980b9"],
+  chart: {
+    height: 350,
+    type: "line",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+  },
+  legend: {
+    position: "top",
+  },
+};
 
-let customer_chart = new ApexCharts(document.querySelector("#customer-chart"), customer_options)
-customer_chart.render()
+let customer_chart = new ApexCharts(
+  document.querySelector("#customer-chart"),
+  customer_options
+);
+customer_chart.render();
 
 setDarkChart = (dark) => {
-    let theme = {
-        theme: {
-            mode: dark ? 'dark' : 'light'
-        }
-    }
+  let theme = {
+    theme: {
+      mode: dark ? "dark" : "light",
+    },
+  };
 
-    customer_chart.updateOptions(theme)
-    category_chart.updateOptions(theme)
-} 
+  customer_chart.updateOptions(theme);
+  category_chart.updateOptions(theme);
+};
 
 // DARK MODE TOGGLE
-let darkmode_toggle = document.querySelector('#darkmode-toggle')
+let darkmode_toggle = document.querySelector("#darkmode-toggle");
 
 darkmode_toggle.onclick = (e) => {
-    e.preventDefault()
-    document.querySelector('body').classList.toggle('dark')
-    darkmode_toggle.querySelector('.darkmode-switch').classList.toggle('active')
-    setDarkChart(document.querySelector('body').classList.contains('dark'))
+  e.preventDefault();
+  document.querySelector("body").classList.toggle("dark");
+  darkmode_toggle.querySelector(".darkmode-switch").classList.toggle("active");
+  setDarkChart(document.querySelector("body").classList.contains("dark"));
+};
+
+let overlay = document.querySelector(".overlay");
+let sidebar = document.querySelector(".sidebar");
+
+document.querySelector("#mobile-toggle").onclick = () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+document.querySelector("#sidebar-close").onclick = () => {
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+// Dropdow
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-let overlay = document.querySelector('.overlay')
-let sidebar = document.querySelector('.sidebar')
-
-document.querySelector('#mobile-toggle').onclick = () => {
-    sidebar.classList.toggle('active')
-    overlay.classList.toggle('active')
-}
-
-document.querySelector('#sidebar-close').onclick = () => {
-    sidebar.classList.toggle('active')
-    overlay.classList.toggle('active')
-}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropicon")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
